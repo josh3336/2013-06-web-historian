3baba7b9bd5823738e56fb6d7f0a714f65f18e6f
Hack Reactor Students <github@hackreactor.com>
2013-07-10 14:44:11 -0700

    client posts to server and server then writes url to file

--- web/basic-server.js
+++ web/basic-server.js
@@ -5,5 +5,6 @@ var port = 8080;
 var ip = "127.0.0.1";
 var server = http.createServer(myStuff.handleRequest);
 console.log("Listening on http://" + ip + ":" + port);
+console.log(myStuff.sum(4,8));
 server.listen(port, ip);
 
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,5 +1,36 @@
 exports.datadir = __dirname + "data/sites.txt"; // tests will need to override this.
+exports.josh="josh";
+exports.sum=function(numa,numb){
+  return numa+numb;
+};
 
 exports.handleRequest = function (req, res) {
+  var body='';
   console.log(exports.datadir);
+
+  if ( req.method==='POST' ){
+    req.on('data',function(chunk){
+      body+=chunk;
+    });
+    req.on('end',function(chunk){
+      body=JSON.parse(body);
+      writetoFile('/Users/hackreactor/code/josh3336/2013-06-web-historian/data/sites.txt', body.address+"\r\n");
+      console.log('end of request, the body is',body);
+    });
+
+  }
+};
+
+
+
+writetoFile= function(location,content){
+  var fs = require('fs');
+  console.log(location,content);
+  fs.appendFile(location, content, function(err) {
+      if(err) {
+          console.log('err');
+      } else {
+          console.log("The file was saved!");
+      }
+  }); 
 };
